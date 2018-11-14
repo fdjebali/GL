75877ac472ab5533e1a2b252e2365c8e19044fd0
firas djebali <firas.djebali@u-psud.fr>
2018-11-14 18:47:35 +0100

    jai fini exo45

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include "TP_lib.h"  
 
 int main(void)
 {
-    return 0;
+
+	unsigned int points=0,resultat,perdu=0;
+	
+	while(perdu!=1)
+	{
+		resultat=LancerDe();
+		if(resultat==1)
+			perdu=1;
+		else if(resultat%2==0)
+			points=points+resultat;
+		else if(resultat==3)
+			points=points*2;
+		else if(resultat==5)
+			points=points-2;
+	}
 }

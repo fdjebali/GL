2bd1b70b345809b1db5cd1d07d544109230e9076
firas djebali <firas.djebali@u-psud.fr>
2018-11-14 18:16:40 +0100

    jai fini exo_44

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include "TP_lib.h"  
 
 int main(void)
 {
-    return 0;
+	short resultat, i=0; 
+	while(i<10)
+	{
+		resultat=LancerDe();
+		i++;
+	}
+	i=0;
 }
+
+//Ces lignes réalisent 10 lancers de dés a 6 faces et on garde seulement le resultat du 10 eme lancer
+//on obtient au debugger resultat=4 et i=10

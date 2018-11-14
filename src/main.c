03787ea261fe7ccc40b3e07d4fca23ee9a451549
firas djebali <firas.djebali@u-psud.fr>
2018-11-14 18:00:11 +0100

    jai fini exo43

--- src/main.c
+++ src/main.c
@@ -2,8 +2,44 @@
 //
 
 #include "my_header.h"
+#include <stdlib.h> 
 
 int main(void)
 {
-    return 0;
+    int s1=3,s2=3,s3=0,s4=0,n=0,n1=0,a=0,rnd=0;
+    while(n<50)
+    {
+        s1=s1+3*n;
+        n=n+1;
+    }
+    while(a<9876)
+    {
+        s2=s2+3*n;
+        a=3*n;
+        n=n+1;
+        
+    }
+	while(n<12)
+	{
+		rnd=rand()%10+1;
+		s3=s3+rnd;
+		n=n+1;
+	}
+    while(n1<20)
+    {
+        if(n%3==0)
+		{
+			s4=s4+n;
+			n1=n1+1;
+		}
+        else if(n%7==0)
+		{
+			s4=s4+n;
+			n1=n1+1;
+        
+        }
+		n=n+1;
+	}
+
+	
 }

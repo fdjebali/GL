d621114b35f0cf3516512e6268644fe777d183e7
firas djebali <firas.djebali@u-psud.fr>
2018-11-15 14:56:18 +0100

    erreurs corrigées exo43

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@
 
 int main(void)
 {
-    int s1=3,s2=3,s3=0,s4=0,n=0,n1=0,a=0,rnd=0;
+    int s1=3,s2=3,s3=0,s4=0,n=2,a=0,rnd=0;
     while(n<50)
     {
         s1=s1+3*n;
         n=n+1;
     }
+    n=2
     while(a<9876)
     {
         s2=s2+3*n;
@@ -25,17 +26,18 @@ int main(void)
 		s3=s3+rnd;
 		n=n+1;
 	}
-    while(n1<20)
+    n=0
+    while(n<20)
     {
         if(n%3==0)
 		{
 			s4=s4+n;
-			n1=n1+1;
+			n=n+1;
 		}
         else if(n%7==0)
 		{
 			s4=s4+n;
-			n1=n1+1;
+			n=n+1;
         
         }
 		n=n+1;

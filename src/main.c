7463cac84788d7e9b1a4607dd640cc6adc016f9a
firas djebali <firas.djebali@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:45:35 +0100

    exo56pasfini

--- src/main.c
+++ src/main.c
@@ -2,8 +2,30 @@
 //
 
 #include "my_header.h"
+#include <stdlib.h>
+#define SEUIL 120
+#define SEUILH 200
+#define SEUILB -100
 
 int main(void)
 {
-    return 0;
+    unsigned char bin[10]={50,60,2,124,85,96,84,127,125,105},i;
+	long int sat[10]={rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand()};
+	
+	for(i=0;i<10;i=i+1)
+	{
+		if(bin[i]<SEUIL)
+			bin[i]=0;
+		else 
+			bin[i]=1;
+	}
+	for(i=0;i<10;i=i+1)
+	{
+		if(sat[i]>=SEUILH)
+			sat[i]=SEUILH;
+		else if(sat[i]<=SEUILB)
+			sat[i]=SEUILB;
+	}
+	i=0;
+		
 }

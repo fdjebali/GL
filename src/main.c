6aaf9e8dac3928b46cdbc91979277af9916a996c
firas djebali <firas.djebali@u-psud.fr>
2018-11-20 20:12:43 +0100

    jai finis exo56

--- src/main.c
+++ src/main.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-    unsigned char bin[10]={50,60,2,124,85,96,84,127,125,105},i;
+    unsigned char bin[10]={50,60,2,124,85,96,84,127,125,105},miroir[10]={1,2,3,4,5,0,0,0,0,0},a[5]={1,5,9,8,11}, b[5]={0,0,0,0,0}i;
 	long int sat[10]={rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand()};
+	char dist[10]={4,9,5,3,7,6,2,4,1,0}; 
 	
 	for(i=0;i<10;i=i+1)
 	{
@@ -26,6 +27,17 @@ int main(void)
 		else if(sat[i]<=SEUILB)
 			sat[i]=SEUILB;
 	}
-	i=0;
-		
+	for(i=1;i<9;i=i+1)
+	{
+		dist[i]=dist[i]-dist[i-1];
+	}
+	for(i=0;i<5;i=i+1)
+	{
+		b[i]=a[4-i];
+	}
+	for(i=0;i<5;i=i+1)
+	{
+		miroir[i+5]=miroir[4-i];
+	}
+	
 }

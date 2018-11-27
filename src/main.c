8f35e4bce0f1158bcfb35d3a8da94af3466c252c
firas djebali <firas.djebali@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:45:34 +0100

    exoavance

--- src/main.c
+++ src/main.c
@@ -2,42 +2,30 @@
 //
 
 #include "my_header.h"
-#include <stdlib.h>
-#define SEUIL 120
-#define SEUILH 200
-#define SEUILB -100
+#include "TP_lib_suite.h"
 
 int main(void)
 {
-    unsigned char bin[10]={50,60,2,124,85,96,84,127,125,105},miroir[10]={1,2,3,4,5,0,0,0,0,0},a[5]={1,5,9,8,11}, b[5]={0,0,0,0,0}i;
-	long int sat[10]={rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand()};
-	char dist[10]={4,9,5,3,7,6,2,4,1,0}; 
+	initialiserTirage(); 
+	short combi_joueur[6]={1,2,3,4,5,6},combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()},i=0,j=0,cpt=0,scoremax=0,n=0;
 	
-	for(i=0;i<10;i=i+1)
+	while(n<100)
 	{
-		if(bin[i]<SEUIL)
-			bin[i]=0;
-		else 
-			bin[i]=1;
+		combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
+		n++;
+	for(i=0;i<6;i++)
+	{
+		for(j=0;j<6;j++)
+		{
+			if(combi_joueur[j]==combi_gagnante[i])
+				cpt++;
+		}
+		if(cpt>scoremax)
+			scoremax=cpt;
 	}
-	for(i=0;i<10;i=i+1)
-	{
-		if(sat[i]>=SEUILH)
-			sat[i]=SEUILH;
-		else if(sat[i]<=SEUILB)
-			sat[i]=SEUILB;
-	}
-	for(i=1;i<9;i=i+1)
-	{
-		dist[i]=dist[i]-dist[i-1];
-	}
-	for(i=0;i<5;i=i+1)
-	{
-		b[i]=a[4-i];
-	}
-	for(i=0;i<5;i=i+1)
-	{
-		miroir[i+5]=miroir[4-i];
 	}
 	
+	i=0;
 }
+    
+

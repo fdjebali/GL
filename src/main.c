8a483ad4f0eca989681ccec4da1ddcb3d1963011
firas djebali <firas.djebali@u-psud.fr>
2018-11-27 19:38:57 +0100

    jai fini exo58

--- src/main.c
+++ src/main.c
@@ -7,25 +7,26 @@
 int main(void)
 {
 	initialiserTirage(); 
-	short combi_joueur[6]={1,2,3,4,5,6},combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()},i=0,j=0,cpt=0,scoremax=0,n=0;
+	short combi_joueur[6]={1,2,3,4,5,6},combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
+	unsigned char i=0,j=0,n=0,cpt=0,scoremax=0;
 	
-	while(n<100)
+	for(n=0;n<100;n++)
 	{
-		combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
-		n++;
 	for(i=0;i<6;i++)
 	{
 		for(j=0;j<6;j++)
 		{
 			if(combi_joueur[j]==combi_gagnante[i])
 				cpt++;
-		}
-		if(cpt>scoremax)
+			if(scoremax<cpt)
 			scoremax=cpt;
+		}
+		
+		
 	}
+	  cpt=0;
 	}
 	
-	i=0;
 }
     
 

142379e2e009087ad06f81d87a4d4e8ffd2c06cd
firas djebali <firas.djebali@u-psud.fr>
2018-12-04 20:42:21 +0100

    exo82 avance

--- src/main.c
+++ src/main.c
@@ -1,31 +1,35 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
+#include <ansi_c.h>
 #include "my_header.h"
-#include "TP_lib_suite.h"
 
 int main(void)
 {
-	unsigned char i=0,j=0,n=0,cpt=0,scoremax=0;
+	 char ligne1[80] = "Nom: pentagone_B" ; 
+	 char ligne2[80] = "Nb points: 5" ; 
+	 char ligne3[80] = "Cordonnees:  10 20 30 20 30 40 43 53 35 65" ; 
+	 char nom_fig[100];
+	 char resultat[100];
+	 char erreur[100] = "Erreur dans les parametres";
+	 int nb_pts=0;
+	 int x[5]={0,0,0,0,0},y[5]={0,0,0,0,0};
+	 
+	 sscanf(ligne1,"%*s %s",nom_fig);
+	 sscanf(ligne2,"%*s %*s %u",&nb_pts);
+	 sscanf(ligne3,"%*s %u %u %u %u %u %u %u %u %u %u",&x[0],&y[0],&x[1],&y[1],&x[2],&y[2],&x[3],&y[3],&x[4],&y[4]);
+	 
+	 if(nb_pts==0 || nom_fig==0)
+		 MessagePopup("erreur",erreur);
+			 
+		 
+	 sprintf(resultat,"La figure %s possède %u sommets",nom_fig,nb_pts);
+	 MessagePopup("phrase",resultat);
+	 
+	 
+
 	
-	for(n=0;n<100;n++)
-	{
-		initialiserTirage(); 
-		short combi_joueur[6]={1,2,3,4,5,6},combi_gagnante[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()}; 
-	for(i=0;i<6;i++)
-	{
-		for(j=0;j<6;j++)
-		{
-			if(combi_joueur[j]==combi_gagnante[i])
-				cpt++;
-			if(scoremax<cpt)
-			scoremax=cpt;
-		}
-		
-		
-	}
-	  cpt=0;
-	}
 	return 0;
 	
 }

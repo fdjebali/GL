39a82ea2250819c6a87112e1840098831033a23b
firas djebali <firas.djebali@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:42:26 +0100

    jai fini exo79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
-    return 0;
+	unsigned char nom[100],prenom[100],chaine[100];
+	unsigned int annee_naissance;
+	
+	printf("nom: ");
+	scanf("%s",nom);
+	printf("prenom: ");
+	scanf("%s",prenom);
+	printf("annee de naissance: ");
+	scanf("%u",&annee_naissance);
+	annee_naissance=2018-annee_naissance;
+	
+	sprintf(chaine,"je m'appelle %s %s et j'ai %u ans",nom,prenom,annee_naissance);
+	MessagePopup("phrase",chaine);
+	
+	
+	return 0;
 }
